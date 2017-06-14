092db5e6e1c978286e2b0b2822a3d8175fd8204e
drinks <drinks.huang@hypers.com>
2017-06-14 11:57:49 +0800

    fix: rename django-cas-sso to cas

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 version = '1.1.5'
 
+
 def recursive_requirements(requirement_file, libs, links, path=''):
     if not requirement_file.startswith(path):
         requirement_file = os.path.join(path, requirement_file)
@@ -12,8 +13,8 @@ def recursive_requirements(requirement_file, libs, links, path=''):
                 requirement_file = requirement.split()[1]
                 if not path:
                     path = requirement_file.rsplit('/', 1)[0]
-                recursive_requirements(requirement_file, libs, links,
-                                       path=path)
+                recursive_requirements(
+                    requirement_file, libs, links, path=path)
             elif requirement.startswith('-f'):
                 links.append(requirement.split()[1])
             elif requirement.startswith('--allow'):
@@ -21,36 +22,37 @@ def recursive_requirements(requirement_file, libs, links, path=''):
             else:
                 libs.append(requirement)
 
+
 libraries, dependency_links = [], []
 recursive_requirements('requirements.txt', libraries, dependency_links)
 
-setup(name='django-cas-sso',
-      version=version,
-      install_requires=libraries,
-      dependency_links=dependency_links,
-      description="Django Cas SSO Client (inherited from django-cas)",
-      long_description=open("./README.md", "r").read(),
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Console",
-          "Intended Audience :: End Users/Desktop",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
-          "Topic :: Utilities",
-          "License :: OSI Approved :: BSD License",
-          ],
-      keywords=['django', 'cas', 'sso'],
-      author='di-dip-unistra',
-      author_email='di-dip@unistra.fr',
-      maintainer='di-dip-unistra',
-      maintainer_email='di-dip@unistra.fr',
-      url='http://github.com/unistra/django-cas/',
-      download_url='http://pypi.python.org/pypi/django-cas-sso',
-      license='MIT',
-      entry_points={},
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=True,
-      )
+setup(
+    name='cas',
+    version=version,
+    install_requires=libraries,
+    dependency_links=dependency_links,
+    description="Django Cas SSO Client (inherited from django-cas)",
+    long_description=open("./README.md", "r").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords=['django', 'cas', 'sso'],
+    author='drinksober',
+    author_email='drinksober@foxmail.com',
+    maintainer='di-dip-unistra',
+    maintainer_email='di-dip@unistra.fr',
+    url='https://github.com/drinksober/django-cas/',
+    download_url='http://pypi.python.org/pypi/django-cas-sso',
+    license='MIT',
+    entry_points={},
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True, )
